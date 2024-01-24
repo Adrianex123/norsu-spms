e5fc935ab38b5929884362c18da507dde98e7087
adrian cardosa <schoolworks554@gmail.com>
2024-01-24 18:04:11 +0700

    logo only

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,18 @@
 import Image from "next/image";
+import norsu from "../images/norsu.png"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Adrian Gwapo</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-0" color="w">
+      <div className="div" >
+      <Image src={norsu} alt="logo" width={150} height={150} />
+
+      <nav>
+        
+      </nav>
+
+      </div>
+     
     </main>
   );
 }
