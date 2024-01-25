0e0003774f419fc6846b2bb6b63c186737b8ce15
adrian cardosa <schoolworks554@gmail.com>
2024-01-25 20:16:42 +0700

    tungaa nix huhu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { Page } from "./form";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center text-center justify-center p-0 bg-dark  w-full">
-      <div className="shadow-xl p-4">
+    <main className="flex min-h-screen  i  w-full">
+      <div className=" p-0">
        
        <Page />
       </div>
