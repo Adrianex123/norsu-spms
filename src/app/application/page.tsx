f613a133cce75dce0a224a2d1eb83faf4abb1aec
Kaihzan-Nixxo C. Jamilla <nixxojam@gmail.com>
2024-03-22 15:16:12 +0800

    Updated

--- src/app/application/page.tsx
+++ src/app/application/page.tsx
@@ -2,7 +2,9 @@
 export default function Dashboard() {
   return (
     <div className="w-full h-full flex justify-center place-items-center">
-      <div className="w-full h-full bg-yellow-100"></div>
+      <div className="w-full h-full bg-yellow-100">
+        
+      </div>
     </div>
   );
 }
